708405a0baf344ff3fe1d5baa7027208a4ca5dd8
Alaka S <alakasunil24@gmail.com>
2025-07-11 18:41:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import subprocess
 import uvicorn
 import asyncio
+from multiprocessing import Process
 from mcp_client import MCPClient
 
 def run_streamlit():
@@ -10,29 +11,35 @@ def run_streamlit():
 def run_fastapi():
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
 
+def run_flask():
+    subprocess.run(["flask", "run", "--app", "flask_app", "--port", "5000"])
+
 def test_client():
     keyword = input("Keyword: ")
     result = asyncio.run(MCPClient().process_query(keyword))
     print(result)
 
 def main():
-    parser = argparse.ArgumentParser(description="Run the product search app in different modes.")
-    parser.add_argument(
-        "mode",
-        choices=["streamlit", "fastapi", "test"],
-        default="fastapi",
-        nargs="?",
-        help="Choose the mode: 'streamlit', 'fastapi', or 'test' (default: fastapi)"
-    )
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mode", choices=["streamlit", "fastapi", "flask", "all", "test"], default="all", nargs="?")
     args = parser.parse_args()
 
     if args.mode == "streamlit":
         run_streamlit()
     elif args.mode == "fastapi":
         run_fastapi()
+    elif args.mode == "flask":
+        run_flask()
     elif args.mode == "test":
         test_client()
+    elif args.mode == "all":
+        p1 = Process(target=run_fastapi)
+        p2 = Process(target=run_flask)
+        p1.start()
+        p2.start()
+        run_streamlit()
+        p1.join()
+        p2.join()
 
 if __name__ == "__main__":
     main()
